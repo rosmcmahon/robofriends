dcc74b73b2a19969bdd6dfea136b371489be24b0
Ros McMahon <rosmcmahon@gmail.com>
2019-05-30 00:41:11 +0700

    Add ErrorBoundary (but comment out Throw Error)

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+	// if(true){
+	// 	throw new Error('AAAAAAAHHHHHHRHRHRGHR!');
+	// }
 	return (
 		<div>
 			{
