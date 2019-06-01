d35c27dadd8c66ffe33cdfd591a0d533ed43da89
Ros McMahon <rosmcmahon@gmail.com>
2019-06-02 03:15:23 +0700

    redux part 1

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css'
 import ErrorBoundary from '../components/ErrorBoundary';
 
+import { setSearchField  } from '../actions';
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+}
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
 class App extends React.Component {
 	constructor(){
 		super();
 		this.state = {
-			robots: [],
-			searchfield: ''
+			robots: []
 		}
 	}
 
@@ -20,20 +33,18 @@ class App extends React.Component {
 			.then(users => this.setState({robots: users}) );
 	}
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
-	}
 	render(){
-		const { robots, searchfield } = this.state;
+		const { robots } = this.state;
+		const { searchField, onSearchChange } = this.props;
 		const filteredRobots = robots.filter( robot =>{
-			return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
+			return robot.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase());
 		});
 		return !robots.length ? 
 			<h1>Loading...</h1> :
 			(
 				<div className='tc'>
 					<h1 className='f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange} />
+					<SearchBox searchChange={onSearchChange} />
 					<Scroll>
 						<ErrorBoundary>
 							<CardList robots={filteredRobots}/>
@@ -44,4 +55,4 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux'
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
 
+
+const store = createStore(searchRobots)
+
 ReactDOM.render(
-	<App />
+	<Provider store={store}>
+		<App/>
+	</Provider>
 	, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
