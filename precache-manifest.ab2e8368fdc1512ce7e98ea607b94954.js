f8c41983ad2c45c8466e13d2499d408bdba90a8d
Ros McMahon <rosmcmahon@gmail.com>
2019-07-26 00:03:22 +0100

    Updates

---
+++ precache-manifest.ab2e8368fdc1512ce7e98ea607b94954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d106690bee3fdef773cc8334d103f3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "20fe6cab485bedbf4e0a",
+    "url": "/robofriends/static/css/main.9256f397.chunk.css"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/js/2.c05ccf0c.chunk.js"
+  },
+  {
+    "revision": "20fe6cab485bedbf4e0a",
+    "url": "/robofriends/static/js/main.e859a7a0.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
